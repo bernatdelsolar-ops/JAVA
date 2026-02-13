6c239589aa6266ab56bc293ed1728c8278d8f0ba
bernatdelsolar-ops <bernatdelsolar@gmail.com>
2026-02-13 01:37:57 +0100

    Add practica_1 Eclipse project and HelloWorld
    Add a new Eclipse Java project 'practica_1' with IDE metadata (.project, .classpath) and .settings configured for Java 17. Include source practica_1/Bernat.java (prints "Hola mundo") and the compiled class in bin as an initial project scaffold for the exercise.

---
+++ practica_1/src/practica_1/Bernat.java
@@ -0,0 +1,13 @@
+package practica_1;
+
+public class Bernat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mundo");
+		
+		
+	}
+
+}
